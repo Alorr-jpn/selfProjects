85aac4f55e120290551e528eccfabf955275b2c0
Alorr-jpn <corasonakoachere@gmail.com>
2023-09-07 09:36:53 +0100

    added a styling sheet using javascript to make DOM features

--- URL/uic.js
+++ URL/uic.js
@@ -1,1 +1,34 @@
-var firstDivision = document.querySelector('.first-division');
+var trial = document.querySelector('.trial');
+var menu = document.querySelector('#menu');
+var file = document.querySelector('#file');
+var cross = document.querySelector('#cross');
+var profile = document.querySelector('.prof1');
+var files = document.querySelector('#files')
+var profiles = document.querySelector('.prof2')
+
+    menu.addEventListener('click', (e) => {
+        trial.style.display = 'inline-block';
+            
+    })
+    cross.addEventListener('click', (e) => {
+        trial.style.display = 'none';
+            
+    })
+    file.addEventListener('click', (e) =>{
+        // file.style.display = 'inline-block'
+        if (profile.style.display == 'inline'){
+            profile.style.display = "none"
+        }else {
+            profile.style.display = "inline"
+        }
+    } )
+
+ files.addEventListener('click', (e) => {
+    if (profiles.style.display == 'block'){
+        profiles.style.display = "none"
+} else {
+    profiles.style.display = "block"
+    
+}
+})
+
